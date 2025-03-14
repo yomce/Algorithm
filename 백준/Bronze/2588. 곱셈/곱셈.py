48f10e6a47fd108758fe1211e567edca1d417995
yomce <149244712+yomce@users.noreply.github.com>
2025-03-14 17:29:35 +0900

    [Bronze III] Title: 곱셈, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -0,0 +1,10 @@
+a = list(input())
+b = list(input())
+
+c = int(a[0])*100+int(a[1])*10+int(a[2])*1
+d = int(b[0])*100+int(b[1])*10+int(b[2])*1
+
+print(c*int(b[2]))
+print(c*int(b[1]))
+print(c*int(b[0]))
+print(c*d)
