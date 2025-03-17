abe104e41649185a326f19a5d638e2d2850a911c
yomce <149244712+yomce@users.noreply.github.com>
2025-03-18 00:53:38 +0900

    [Bronze III] Title: 팩토리얼, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10872. 팩토리얼/팩토리얼.py
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.py
@@ -1,6 +1,16 @@
-N = int(input())  
-result = 1
-for i in range(1,N+1):
-  result *= i
+# #10872 팩토리얼
+# N = int(input())  
+# result = 1
+# for i in range(1,N+1):
+#   result *= i
     
-print(result)
+# print(result)
+
+def factorial(n: int) -> int:
+    if n > 0:
+        return n*factorial(n-1)
+    else:
+        return 1
+
+n = int(input())
+print(factorial(n))
