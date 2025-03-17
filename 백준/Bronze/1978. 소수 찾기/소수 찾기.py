19c108d35e5890d2d56a18643d63e1c498226efb
yomce <149244712+yomce@users.noreply.github.com>
2025-03-17 13:23:02 +0900

    [Bronze II] Title: 소수 찾기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -0,0 +1,33 @@
+def find_prime_num(n:int = 1000):
+    """
+    find list of prime number under num 'n'
+    """
+    
+    prime_num_list = [2]
+    
+    for num in range(3, n+1):
+        
+        for prime_number in prime_num_list:
+            if num % prime_number == 0:  # not frame, next num
+                break
+            
+            if prime_num_list[-1] == prime_number:
+                prime_num_list.append(num)
+        
+    return prime_num_list
+
+
+
+
+num_test_case = int(input())
+
+test_list = list(map(int, input().split()))
+
+num_prime = 0
+list_all_prime_num = find_prime_num(n = 1000)
+
+for test_case in test_list:
+    if test_case in list_all_prime_num:
+        num_prime += 1
+
+print(num_prime)
