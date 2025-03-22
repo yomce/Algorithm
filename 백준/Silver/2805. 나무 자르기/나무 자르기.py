7a5eaf13d4aede49e5e913d265aad9ee0bcd91d5
yomce <149244712+yomce@users.noreply.github.com>
2025-03-22 10:04:08 +0900

    [Silver II] Title: 나무 자르기, Time: 2128 ms, Memory: 148224 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+trees = list(map(int, input().split()))
+# trees.sort()  # 있어도 되고 없어도 됨
+
+def get_cut_sum(trees, h):
+    return sum((tree - h) for tree in trees if tree > h)
+
+def binary_search(trees, target):
+    left = 0
+    right = max(trees)
+    result = 0
+
+    while left <= right:
+        mid = (left + right) // 2
+        total = get_cut_sum(trees, mid)
+
+        if total >= target:
+            result = mid
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return result
+
+print(binary_search(trees, M))
