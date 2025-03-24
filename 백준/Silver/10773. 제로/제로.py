7e071bb7bcbc93aa6df6dc3747ecff6044ea7889
yomce <149244712+yomce@users.noreply.github.com>
2025-03-24 12:22:38 +0900

    [Silver IV] Title: 제로, Time: 72 ms, Memory: 33192 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.py
@@ -0,0 +1,14 @@
+import sys
+
+k = int(sys.stdin.readline())
+stack = []
+
+for _ in range(k):
+    num = int(sys.stdin.readline())
+    if num == 0:
+        if stack:
+            stack.pop()
+    else:
+        stack.append(num)
+
+print(sum(stack))
